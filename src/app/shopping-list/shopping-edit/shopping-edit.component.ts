5681ad2408cc3c2ccd28376798b3661ad7e469d0
mercuryaura <mercuryauraprima@gmail.com>
2018-11-01 17:29:34 +0700

    first commit

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from './ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+
+  @ViewChild('nameInput') nameRef:ElementRef;
+  @ViewChild('amountInput') amountRef:ElementRef;
+  @Output() ingredientAdded= new EventEmitter<Ingredient>();
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddItem(){
+    const ingName=this.nameRef.nativeElement.value;
+    const ingAmount=this.amountRef.nativeElement.value;
+    const newIngredient=new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
+}
